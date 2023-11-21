aaf7dd5e9ed4ad93977cff84f70b45d0d877abc3
Saad Muhammad Khan <saadkhanhere@gmail.com>
2023-11-22 01:01:53 +0500

    first commit

---
+++ Snippets.py
@@ -0,0 +1,16 @@
+# Arithmetic Operators:
+# Addition:         3 + 2
+# Subtraction:      3 - 2
+# Multiplication:   3 * 2
+# Division:         3 / 2
+# Floor Division:   3 // 2
+# Exponent:         3 ** 2
+# Modulus:          3 % 2
+
+# Comparison:
+# Equal:                3 == 2
+# No Equal:             3 != 2
+# Greater Than:         3 > 2
+# Less Than:            3 < 2
+# Greater Than Equal:   3 >= 2
+# Less Than Equal:      3 <= 2
